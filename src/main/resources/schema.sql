fe94a33fa4d2f25d4e4c64c2521025ae3cf9d873
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-18 22:37:57 +0500

    feat: create tables income_categories & expense_categories

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+--таблица с категориями доходов
+CREATE TABLE income_categories
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(15)
+);
+
+--Таблица с категориями расходов
+CREATE TABLE expense_categories
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(15)
+);
