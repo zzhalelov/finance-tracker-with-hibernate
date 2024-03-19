f132d165d4bc312d3c61fce99ba0a08ca0f5bc8c
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-19 22:57:23 +0500

    feat: add method that print all expenses categories

--- src/main/java/CreateExpense.java
+++ src/main/java/CreateExpense.java
@@ -15,6 +15,7 @@ public class CreateExpense {
 
         LocalDate date = getDate("Введите дату:");
         double amount = getDouble("Введите сумму расхода:");
+        printAllCategories();
 
     }
 
@@ -26,5 +27,22 @@ public class CreateExpense {
     static double getDouble(String message) {
         System.out.println(message);
         return Double.parseDouble(scanner.nextLine());
+    }
+
+    static void printAllCategories() {
+        System.out.println("----------------------");
+        System.out.println("КАТЕГОРИИ:");
+        System.out.println("1. Питание");
+        System.out.println("2. Жилье");
+        System.out.println("3. Коммунальные услуги");
+        System.out.println("4. Обучение и саморазвитие");
+        System.out.println("5. Здоровье и красота");
+        System.out.println("6. Подарки и развлечения");
+        System.out.println("7. Транспортные расходы");
+        System.out.println("8. Связь и интернет");
+        System.out.println("9. Техника");
+        System.out.println("10. Одежда");
+        System.out.println("11. Фин.обязательства");
+        System.out.println("12. Прочие расходы");
     }
 }
