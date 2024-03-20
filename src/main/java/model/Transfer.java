34a0ccabf80d40e5983aaf937edb99f7aea0892a
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-20 21:57:44 +0500

    feat: create class Transfer

---
+++ src/main/java/model/Transfer.java
@@ -0,0 +1,30 @@
+package model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "transfers")
+public class Transfer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private LocalDate date;
+    private double amount;
+    private double fee;
+
+    @Column(name = "source_account_id")
+    private int sourceAccountId;
+
+    @Column(name = "destination_account_id")
+    private int destinationAccountId;
+
+    private String comment;
+}
