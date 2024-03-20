6978947acdbc51224b365a56f704f43699a71e18
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-20 13:34:17 +0500

    fix: change hibernate version to 5.6.15

--- src/main/java/CreateExpense.java
+++ src/main/java/CreateExpense.java
@@ -1,8 +1,6 @@
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.time.LocalDate;
 import java.util.Scanner;
 
