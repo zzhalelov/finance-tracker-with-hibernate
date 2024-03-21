9719b34bc1337b1661d72ead75a271cbdfbadfe0
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-21 22:48:05 +0500

    feat: add methods that calculate sum of all transactions & sum of all transactions between two dates

--- src/main/java/dao/TransactionDao.java
+++ src/main/java/dao/TransactionDao.java
@@ -6,6 +6,8 @@ import model.Income;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.time.LocalDate;
 import java.util.List;
 
 public class TransactionDao {
@@ -19,5 +21,22 @@ public class TransactionDao {
 
     public List<Income> printAllIncomes() {
         return manager.createQuery("SELECT i FROM Income i", Income.class).getResultList();
+    }
+
+    //2. Вывести сумму по всем доходам и расходам
+    public Double sumAllIncomes() {
+        return manager.createQuery("SELECT SUM (amount) FROM Income i", Double.class).getSingleResult();
+    }
+
+    public Double sumAllExpenses() {
+        return manager.createQuery("SELECT SUM (amount) FROM Expense e", Double.class).getSingleResult();
+    }
+
+    //3. Вывести сумму доходов и расходов за промежуток времени, указанный пользователем
+    public Double sumExpensesBetweenDate(LocalDate startDate, LocalDate endDate) {
+        TypedQuery<Double> query = manager.createQuery("SELECT SUM(e.amount) FROM Expense e WHERE e.date BETWEEN :startDate AND :endDate", Double.class);
+        query.setParameter("startDate", startDate);
+        query.setParameter("endDate", endDate);
+        return query.getSingleResult();
     }
 }
