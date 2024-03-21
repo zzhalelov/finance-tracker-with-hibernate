0442bb01c6f3ad477ea41c663ed7ebc9bcc2d7bf
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-21 13:59:03 +0500

    feat: add methods printAllAccounts; Transfer builder

--- src/main/java/CreateTransfer.java
+++ src/main/java/CreateTransfer.java
@@ -1,3 +1,7 @@
+import model.Account;
+import model.Expense;
+import model.Transfer;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -13,6 +17,25 @@ public class CreateTransfer {
 
         LocalDate date = getDate("Введите дату в формате yyyy-mm-dd:");
         double amount = getDouble("Введите сумму дохода:");
+        double fee = getDouble("Введите сумму комиссии за перевод:");
+
+        printAllAccounts();
+        int sourceAccountId = getInt("С какого счета переводите деньги:");
+        manager.find(Account.class, sourceAccountId);
+        int destinationAccountId = getInt("На какой счет переводите деньги:");
+        manager.find(Account.class, destinationAccountId);
+
+        String comment = getComment("Введите описание транзакции:");
+
+        Transfer transfer = Transfer.builder()
+                .date(date)
+                .amount(amount)
+                .fee(fee)
+                .sourceAccountId(sourceAccountId)
+                .destinationAccountId(destinationAccountId)
+                .comment(comment)
+                .build();
+
     }
 
     static LocalDate getDate(String message) {
@@ -23,5 +46,21 @@ public class CreateTransfer {
     static double getDouble(String message) {
         System.out.println(message);
         return Double.parseDouble(scanner.nextLine());
+    }
+
+    static void printAllAccounts() {
+        System.out.println("----------------------");
+        System.out.println("1. Наличные");
+        System.out.println("2. Банковская карта");
+    }
+
+    static int getInt(String message) {
+        System.out.println(message);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    static String getComment(String message) {
+        System.out.println(message);
+        return scanner.nextLine();
     }
 }
