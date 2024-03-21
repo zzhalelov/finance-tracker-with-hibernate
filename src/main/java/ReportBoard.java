a6c1b642e64f4b04523962bded364ad03b7f3ae8
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-21 22:58:31 +0500

    feat: add case 2 & 3

--- src/main/java/ReportBoard.java
+++ src/main/java/ReportBoard.java
@@ -15,6 +15,8 @@ public class ReportBoard {
                     return;
                 }
                 case 1 -> transactionService.printAll();
+                case 2 -> transactionService.sumAll();
+                case 3 -> transactionService.sumAllBetweenDate();
             }
         }
     }
@@ -22,6 +24,8 @@ public class ReportBoard {
     public static void menu() {
         System.out.println("-----------------------------------------------");
         System.out.println("1. Вывести список доходов и расходов за весь период");
+        System.out.println("2. Вывести сумму по всем доходам и расходам");
+        System.out.println("3. Вывести сумму доходов и расходов за промежуток времени, указанный пользователем");
         System.out.println("-----------------------------------------------");
     }
 }
