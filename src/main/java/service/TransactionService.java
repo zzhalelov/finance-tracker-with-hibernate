59fca0077412c254a9f697e2d829b6ebc6b9d134
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-03-21 22:58:05 +0500

    feat: add scanner

--- src/main/java/service/TransactionService.java
+++ src/main/java/service/TransactionService.java
@@ -4,9 +4,13 @@ import dao.TransactionDao;
 import model.Expense;
 import model.Income;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 public class TransactionService {
+    Scanner scanner = new Scanner(System.in);
+
     //1. Вывести список всех доходов и расходов
     public void printAll() {
         TransactionDao dao = new TransactionDao();
@@ -27,5 +31,21 @@ public class TransactionService {
                 System.out.println(expense);
             }
         }
+    }
+
+    //2. Вывести сумму по всем доходам и расходам
+    public void sumAll() {
+        TransactionDao dao = new TransactionDao();
+        System.out.println("Доходов всего: " + dao.sumAllIncomes());
+        System.out.println("Расходов всего: " + dao.sumAllExpenses());
+    }
+
+    //3. Вывести сумму доходов и расходов за промежуток времени, указанный пользователем
+    public void sumAllBetweenDate() {
+        TransactionDao dao = new TransactionDao();
+        System.out.println("Введите начальную и конечную дату для отбора: ");
+        LocalDate startDate = LocalDate.parse(scanner.nextLine());
+        LocalDate endDate = LocalDate.parse(scanner.nextLine());
+        System.out.println(dao.sumExpensesBetweenDate(startDate, endDate));
     }
 }
